fab2ea3c4c9e9012167646d946ca5b335cbbfd7e
Avish <avishmehta68@gmail.com>
2024-04-12 11:50:07 +0530

    Worked on Class and constuctor

---
+++ day4/ApplicationBasicsOfCs/BasicsOfCs/Employee.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsOfCs
+{
+    class Employee
+    {
+        // int id;
+        //public int GetId()
+        //{
+        //    return id;
+        //}
+        //public void PutId(int eid)
+        //{
+        //    id = eid;
+        //}
+        //public int Id
+        //{
+        //    get { return id; }
+        //    set { id = value; }
+        //}
+        //public int Id
+        //{
+        //    get => id;
+        //    set => id = value;
+        //}
+        public int Id { get; set; }
+        public string Name { get; set; }
+        double salary;
+        public double Salary
+        {
+            set
+            {
+                salary = value;
+            }
+            get
+            {
+                return salary-salary*10/100;
+            } 
+        }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+
+        // default constructor 
+        public Employee()
+        {
+            Id = 0; 
+            Name = string.Empty;
+            Email = string.Empty;
+            DateOfBirth = DateTime.MinValue;
+            Salary = 0; 
+        }
+        // parameterized constructor
+        public Employee(int id)
+        {
+            Id = id;
+        }
+
+        public Employee(int id, string name, double salary, DateTime dateOfBirth, string email) : this(id) // constuctor chaining 
+        {
+            Name = name;
+            Salary = salary;
+            DateOfBirth = dateOfBirth;
+            Email = email;
+        }
+
+        public void Work()
+        {
+            Id = 101;
+            Console.WriteLine("Work");
+        }
+    }
+}
---
+++ day4/ApplicationBasicsOfCs/BasicsOfCs/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace BasicsOfCs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Employee employee = new Employee();
+            employee.Id = 1000;
+            employee.Name = "Avish";
+            Console.WriteLine(employee.Id);
+            Console.WriteLine(employee.Name);
+
+            Employee employee2 = new Employee() {
+                Id = 1001,
+                Name = "Ramu",
+                Salary = 40000,
+                DateOfBirth = new DateTime(2000, 08, 12),
+                Email="ramu@abc.com"
+
+            };
+            Console.WriteLine(employee2.Id);
+            Console.WriteLine(employee2.Name);
+            Console.WriteLine(employee2.Salary);
+            Console.WriteLine(employee2.DateOfBirth);
+            Console.WriteLine(employee2.Email);
+
+            Employee employee3 = new Employee(1002, "nanam", 50000, new DateTime(2000, 5, 20), "naman@gmail.com");
+            Console.WriteLine(employee3.Salary);
+
+            employee.Work();
+
+        }
+    }
+}
