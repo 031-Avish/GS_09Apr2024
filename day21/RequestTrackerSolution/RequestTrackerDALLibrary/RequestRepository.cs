4a93b0f6845587e46442c8acd97cccb7a8b52c65
Avish <avishmehta68@gmail.com>
2024-05-09 17:21:44 +0530

    Day 21

---
+++ day21/RequestTrackerSolution/RequestTrackerDALLibrary/RequestRepository.cs
@@ -0,0 +1,39 @@
+﻿//using RequestTrackerModelLibrary;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace RequestTrackerDALLibrary
+//{
+//    public class RequestRepository : IRepository<int, Employee>
+//    {
+//        private readonly RequestTrackerContext _context;
+
+//        public Task<Employee> Add(Employee entity)
+//        {
+//            _constext = new RequestTrackerContext();
+//        }
+
+//        public Task<Employee> Delete(int key)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public Task<Employee> Get(int key)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public Task<IList<Employee>> GetAll()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public Task<Employee> Update(Employee entity)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
