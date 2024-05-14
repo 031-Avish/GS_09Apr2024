72fa939573043e296e18c4efb5726b1356e18ad7
Avish <avishmehta68@gmail.com>
2024-05-14 17:21:36 +0530

    added doctor controller

---
+++ day24/DoctorAppointmentSolution/DoctorAppointmentApp/Controllers/DoctorController.cs
@@ -0,0 +1,61 @@
+﻿using DoctorAppointmentApp.Exceptions;
+using DoctorAppointmentApp.Interfaces;
+using DoctorAppointmentApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoctorAppointmentApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorController: ControllerBase 
+    {
+        private readonly IDoctorService _doctorService;
+        public DoctorController(IDoctorService doctorService)
+        { 
+            _doctorService = doctorService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IList<Doctor>>> Get()
+        {
+            try
+            {
+                var doctors=await _doctorService.GetAllDoctor();
+                return Ok(doctors.ToList());
+
+            }catch(NoDoctorsFoundException ndfe)
+            {
+                return NotFound(ndfe.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Doctor>> Put(int id, float experience)
+        {
+            try
+            {
+                var doctor = await _doctorService.UpdateDoctorExperience(id, experience);
+                return Ok(doctor);  
+            }
+            catch(NoSuchDoctorException nsde)
+            {
+                return NotFound(nsde.Message);
+            }
+        }
+
+        [Route("GetAllDoctorBySpecialization")]
+        [HttpGet]
+        public async Task<ActionResult<IList<Doctor>>> Get(string Specialization)
+        {
+            try
+            {
+                var doctor = await _doctorService.GetDoctorsBySpecialization(Specialization);
+                return Ok(doctor.ToList());
+            }
+            catch(NoSuchDoctorException nsde)
+            {
+                return NotFound(nsde.Message);
+            }
+        }
+    }
+}
--- day24/DoctorAppointmentSolution/DoctorAppointmentApp/Interfaces/IDoctorService.cs
+++ day24/DoctorAppointmentSolution/DoctorAppointmentApp/Interfaces/IDoctorService.cs
@@ -4,7 +4,7 @@ namespace DoctorAppointmentApp.Interfaces
 {
     public interface IDoctorService
     {
-        public Task<IEnumerable<Doctor>> GetDoctorsBySpeciality(string speciality);
+        public Task<IEnumerable<Doctor>> GetDoctorsBySpecialization(string Specialization);
         public Task<Doctor> UpdateDoctorExperience(int doctorId, float experience);
         public Task<IEnumerable<Doctor>> GetAllDoctor();
     }
